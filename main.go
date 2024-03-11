80fee4d248cd343412f7b5b013b54cca0fe33aa8
web3santa <usefulstory35@gmail.com>
2024-03-12 01:29:09 +0900

    add user info

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "log"
+import (
+	"fmt"
+)
 
 func main() {
-	log.Println("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are stil avilable\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+
 }
