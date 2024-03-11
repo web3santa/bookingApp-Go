a1e8a2bc7c5a7a7a31884328d18e78ce52306b37
web3santa <usefulstory35@gmail.com>
2024-03-12 01:58:18 +0900

    add array

--- main.go
+++ main.go
@@ -5,20 +5,40 @@ import (
 )
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
+	var bookings [50]string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are stil avilable\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var fistName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// ask user for their name
-	userName = "Tom"
-	userTickets = 2
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&fistName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter your ticket: ")
+	fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = fistName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value : %v\n", bookings[0])
+	fmt.Printf("The Type : %T\n", bookings)
+	fmt.Printf("The array elngth : %v\n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. you will receive a confirmation email at %v \n",
+		fistName, lastName, userTickets, email)
+
+	fmt.Printf("%v tickets remaing for %v\n", remainingTickets, conferenceName)
 
 }
